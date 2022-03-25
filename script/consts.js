3b7da36ab22e6e3003d4f1656402e0ac62cbd056
isaackwok <kch062522@gmail.com>
2022-03-25 13:24:35 +0800

    style: rename variable

--- script/consts.js
+++ script/consts.js
@@ -1,5 +1,5 @@
 /* Constants */
 const MD_FIELD_CLASSNAME = '.js-vue-markdown-field'
-const MD_PREVIEW_AREA_CLASS = 'mdPreviewArea'
-const MD_PREVIEW_AREA_SELECTOR = `div.${MD_PREVIEW_AREA_CLASS}`
+const MD_PREVIEW_AREA_CLASSNAME = '.mdPreviewArea'
+const MD_PREVIEW_AREA_SELECTOR = `div${MD_PREVIEW_AREA_CLASS}`
 const PREVIEW_API_URL = `${getApiBasepath(document.URL)}/preview_markdown`
--- script/index.js
+++ script/index.js
@@ -3,6 +3,6 @@ const mdFields = document.querySelectorAll(MD_FIELD_CLASSNAME)
 
 mdFields.forEach((field) => {
     const inputArea = field.querySelector('textarea')
-    inputArea.parentElement?.appendChild(createPreviewDiv(MD_PREVIEW_AREA_CLASS))
+    inputArea.parentElement?.appendChild(createPreviewDiv(MD_PREVIEW_AREA_CLASSNAME))
     inputArea.oninput = handleOnChangePreview
 })
